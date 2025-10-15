6bb523fddee1f08f23960befe3fdc6cac46ba0a1
Sahil Gulati <gulatisahil599@gmail.com>
2025-10-15 22:51:16 +0530

    dss_added

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Server you want to monitor
-const targetServer = 'https://brochat-brokebro.onrender.com/health';
+const targetServer = 'https://brochat-2cqd.onrender.com/health';
+const targetServer2 = 'https://crop-service-wkxt.onrender.com/health';
 
 // Function to check health of BroChat
 const checkHealth = async () => {
   try {
     await axios.get(targetServer);
     console.log(`[✅ Healthy] ${targetServer} at ${new Date().toLocaleTimeString()}`);
+    await axios.get(targetServer2);
+    console.log(`[✅ Healthy] ${targetServer2} at ${new Date().toLocaleTimeString()}`);
   } catch (error) {
     console.error(`[❌ Down] ${targetServer} at ${new Date().toLocaleTimeString()} - ${error.message}`);
+    console.error(`[❌ Down] ${targetServer2} at ${new Date().toLocaleTimeString()} - ${error.message}`);
   }
 };
 
